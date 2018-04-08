820ed3e96b65096224d12130bf02a1384c038646
saied07 <38184314+saied07@users.noreply.github.com>
2018-04-08 16:49:35 +0200

    Add files via upload

---
+++ js/Slick.js
@@ -0,0 +1,26 @@
+/**
+ * Created by Saied on 10/23/2017.
+ */
+
+/*
+ var div1=document.getElementById('div1');
+ var div2=document.getElementById('div2');
+   if (div2.clientHeight>div1.clientHeight)
+      {
+          div2.style.height=div1.clientHeight+'px';
+          console.log("The height of image div is "+images.clientHeight+'px');
+          console.log("The height of paragraph div is "+paragraphs.clientHeight+'px');
+        //  alert("The height of image is equal "+images.clientHeight+" & The height of the paragraph is equal "+paragraphs.clientHeight);
+      }
+      else {
+       paragraphs.style.height=images.clientHeight+'px';
+       console.log("The height of image div is "+images.clientHeight+'px');
+       console.log("The height of paragraph div is "+paragraphs.clientHeight+'px');
+       //   alert("The height of image is equal"+images.clientHeight+" The height of the paragraph is equal "+paragraphs.clientHeight);
+      }
+*/
+$(document).ready(function(){
+    $(".send").click(function(){
+        alert(innerWidth);
+    })
+})
